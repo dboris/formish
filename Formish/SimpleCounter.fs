59e3864cca8dc386dabc94b25199447bbaaf3008
Boris D. <boris.dob@gmail.com>
2017-08-31 15:23:14 -0700

    Refactor.

--- Formish/SimpleCounter.fs
+++ Formish/SimpleCounter.fs
@@ -16,27 +16,35 @@ module SimpleCounter =
         | Increment -> { model with x = model.x + 1 }, Cmd.none
         | Decrement -> { model with x = model.x - 1 }, Cmd.none
 
-    let label = Label (XAlign = TextAlignment.Center)
-    let incButton = Button (Text = "Increment")
-    let decButton = Button (Text = "Decrement")
-    let stack = StackLayout (VerticalOptions = LayoutOptions.Center)
-    do
-        stack.Children.Add label
-        stack.Children.Add incButton
-        stack.Children.Add decButton
-    let page = ContentPage (Content = stack)
+    type CounterPage () =
+        inherit ContentPage ()
+        let label = Label (XAlign = TextAlignment.Center)
+        let incButton = Button (Text = "Increment")
+        let decButton = Button (Text = "Decrement")
+        let stack = StackLayout (VerticalOptions = LayoutOptions.Center)
+        do
+            stack.Children.Add label
+            stack.Children.Add incButton
+            stack.Children.Add decButton
+            base.Content <- stack
+
+        member __.SubscribeHandlers initial =
+            let sub dispatch =
+                incButton.Clicked.Add (fun _ -> dispatch Increment)
+                decButton.Clicked.Add (fun _ -> dispatch Decrement)
+            Cmd.ofSub sub
+
+        member __.Counter
+            with get () = label.Text
+            and set value = label.Text <- value
+
+    let page = CounterPage ()
 
     let view model dispatch =
-        Device.BeginInvokeOnMainThread (fun () -> label.Text <- sprintf "%i" model.x)
-
-    let subHandlers initial =
-        let sub dispatch =
-            incButton.Clicked.Add (fun _ -> dispatch Increment)
-            decButton.Clicked.Add (fun _ -> dispatch Decrement)
-        Cmd.ofSub sub
+        Device.BeginInvokeOnMainThread (fun () -> page.Counter <- sprintf "%i" model.x)
 
     let run () =
         Program.mkProgram init update view
-        |> Program.withSubscription subHandlers
+        |> Program.withSubscription page.SubscribeHandlers
         |> Program.withConsoleTrace
         |> Program.run
